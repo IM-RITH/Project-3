f17620b9c902841b990a23292798978bdca6acaf
RITH <srey.sovanrithhust@gmail.com>
2023-11-18 21:33:53 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminHomePageController;
+use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Controllers\Admin\AdminProfileController;
+use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\TermsController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('terms', [TermsController::class, 'index'])->name('terms');
+
+/*Admin route*/
+Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+Route::post('/admin/login-submit', [AdminLoginController::class, 'login_submit'])->name('admin_login_submit');
+Route::get('/admin/logout', [AdminLoginController::class, 'logout'])->name('admin_logout');
+Route::get('/admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
+Route::post('/admin/forget-password-submit', [AdminLoginController::class, 'forget_password_submit'])->name('admin_forget_password_submit');
+Route::get('/admin/reset-password/{token}/{email}', [AdminLoginController::class, 'reset_password'])->name('admin_reset_password');
+Route::post('/admin/reset-password-submit', [AdminLoginController::class, 'reset_password_submit'])->name('admin_reset_password_submit');
+
+Route::middleware(['admin:admin'])->group(function () {
+    Route::get('/admin/home', [AdminHomeController::class, 'index'])
+        ->name('admin_home');
+    Route::get('/admin/edit-profile', [AdminProfileController::class, 'index'])
+        ->name('admin_profile');
+    Route::post('/admin/edit-profile-submit', [AdminProfileController::class, 'profile_submit'])
+        ->name('admin_profile_submit');
+    Route::get('/admin/home-page', [AdminHomePageController::class, 'index'])
+        ->name('admin_home_page');
+    Route::post('/admin/home-page/update', [AdminHomePageController::class, 'update'])
+        ->name('admin_home_page_update');
+});
